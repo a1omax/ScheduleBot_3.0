ab5f3e40843d11aaebfa108f5ddfbc95a2857a19
a1omax <53946133+a1omax@users.noreply.github.com>
2021-03-18 10:59:52 +0200

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -4,25 +4,25 @@ import telebot
 from dateutil.tz import tzoffset
 import sqlite3
 
-
 bot = telebot.TeleBot(TOKEN)
 
 # static vars time unused
-para_start = [[7, 30], [9, 0],   [10, 30], [12, 30], [14, 0],  [15, 30], [16, 50], [18, 20]]
-para_finish = [[8, 50], [10, 20], [11, 50], [13, 50], [15, 20], [16, 50], [18, 10],  [19, 40]]
+para_start = [[7, 30], [9, 0], [10, 30], [12, 30], [14, 0], [15, 30], [16, 50], [18, 20]]
+para_finish = [[8, 50], [10, 20], [11, 50], [13, 50], [15, 20], [16, 50], [18, 10], [19, 40]]
 
 break_start = [[8, 50], [10, 20], [11, 50], [13, 50], [15, 20], [16, 50], [18, 10]]
-break_finish =[[9, 0],  [10, 30], [12, 30], [14, 0],  [15, 30],[16, 50], [18, 20]]
+break_finish = [[9, 0], [10, 30], [12, 30], [14, 0], [15, 30], [16, 50], [18, 20]]
 
 dict_days = {
-        0: ['monday', 'понедельник', 'пн', 'понеділок'],
-        1: ['tuesday', 'вторник', 'вт', 'вівтор'],
-        2: ['wednesday', 'среда' ,'сред', 'ср', 'серед'],
-        3: ['thursday', 'четверг', 'чт', 'четвер'],
-        4: ['friday', 'пятница', 'пт', "п'ятниц", "пятниц"],
-        5: ['saturday', 'суббота', 'сб ', 'субот', 'cуббот'],
-        6: ['sunday', 'воскресенье', 'вс ', 'воскресенье', 'неділ']
-    }
+    0: ['monday', 'понедельник', 'пн', 'понеділок'],
+    1: ['tuesday', 'вторник', 'вт', 'вівтор'],
+    2: ['wednesday', 'среда', 'сред', 'ср', 'серед'],
+    3: ['thursday', 'четверг', 'чт', 'четвер'],
+    4: ['friday', 'пятница', 'пт', "п'ятниц", "пятниц"],
+    5: ['saturday', 'суббота', 'сб ', 'субот', 'cуббот'],
+    6: ['sunday', 'воскресенье', 'вс ', 'воскресенье', 'неділ']
+}
+
 
 def time_update():
     timezone = 2
@@ -33,8 +33,10 @@ def time_update():
     minute = int(now.strftime("%M"))
     date = now.strftime("%m/%d/%Y, %H:%M:%S")
 
+
 def the_day(value=0):
     return (now.weekday() + value) % 7
+
 
 con = sqlite3.connect("schedule.db")
 cur = con.cursor()
@@ -48,7 +50,6 @@ con.commit()
 
 
 def check_group(msg):
-
     con = sqlite3.connect("schedule.db")
     cur = con.cursor()
 
@@ -61,13 +62,13 @@ def check_group(msg):
 
 
 def set_group(msg):
+
     con = sqlite3.connect("schedule.db")
     cur = con.cursor()
     tg_id = str(msg.from_user.id)
 
-    gr_name = cur.execute("""SELECT group_name FROM schedule""").fetchall()
+    gr_name = cur.execute("""SELECT DISTINCT group_name FROM schedule""").fetchall()
     answer = (str(msg.text)).split(" ")
-
     for i in answer:
         for j in gr_name:
             if i == j[0]:
@@ -79,23 +80,37 @@ def set_group(msg):
                     cur.execute("""INSERT INTO users VALUES(?,?,?,?)""", (msg.from_user.id, msg.from_user.username,
                                                                           i, date))
 
-                bot.reply_to(msg, ("Группа успешно установлена на " + i))
+                bot.reply_to(msg, "Группа успешно установлена на " + i, parse_mode="Markdown")
                 con.commit()
-                return i
+                return 1
 
     else:
-        bot.reply_to(msg, "Данной группы нет в базе или другая ошибка.")
+        suggests = ""
+        for i in answer:
+            i = "%" + i + "%"
+            grp = cur.execute(
+                """SELECT DISTINCT group_name FROM schedule WHERE group_name LIKE (?) ORDER BY group_name""",
+                (i,)).fetchall()
+            if grp:
+                for g in grp:
+                    suggests += "`" + g[0] + "`    "
+        if suggests:
+            bot.reply_to(msg, "Данной группы нет в базе. Может вы имели в виду: \n" \
+                         + suggests + "\nПожалуйста, повторите попытку, нажав команду /group и укажите группу одну " \
+                                      "из списка _(в точности как указано в списке)_", parse_mode="Markdown")
+            return 2
+
         return 0
 
 
 def week_now(change=0):
-
-    week = ( int(now.strftime("%V")) + ((int(now.strftime("%d")) + change) // 7)) % 2
+    week = (int(now.strftime("%V")) + ((int(now.strftime("%d")) + change) // 7)) % 2
     # Учебный год начинаться с четной недели (не совпадает)
     if week == 1:
         return "even"
     elif week == 0:
         return "odd"
+
 
 def write_tg(first_gr, second_gr, day, message):
     to_write = "*" + dict_days[int(day)][1].capitalize() + "*"
@@ -118,10 +133,11 @@ def write_tg(first_gr, second_gr, day, message):
                 else:
                     to_write += "Нет пары"
             else:
-                to_write += "\n\n*"+str(first_gr[i][0])+" пара:*\nНет пары"
+                to_write += "\n\n*" + str(first_gr[i][0]) + " пара:*\nНет пары"
         bot.reply_to(message, to_write, parse_mode='Markdown')
     else:
         bot.reply_to(message, "У вас нет пар")
+
 
 def sched_named_day(message, day):
     grp = check_group(message)
@@ -185,13 +201,13 @@ def sched_by_day(message, day_change):
 def para_today(message):
     sched_by_day(message, 0)
 
+
 @bot.message_handler(commands=['tomorrow', 'завтра'])
 def para_tomorrow(message):
     sched_by_day(message, 1)
 
 
 def week_day(message):
-
     for word in message.text.lower().split(" "):
         for day in dict_days:
             for i in dict_days[day]:
@@ -201,7 +217,6 @@ def week_day(message):
 
 
 def check(message):
-
     day = week_day(message)
 
     if day != -1:
@@ -209,9 +224,9 @@ def check(message):
     else:
         bot.reply_to(message, "Такого дня нет")
 
+
 @bot.message_handler(commands=['day'])
 def para_named_day(message):
-
     day = week_day(message)
     if day != -1:
         sched_named_day(message, day)
@@ -221,12 +236,19 @@ def para_named_day(message):
         bot.register_next_step_handler(msg, check)
 
 
+def check_grp(msg):
+
+    reply = set_group(msg)
+    if reply == 0:
+        bot.reply_to(msg, "Неправильно указана группа или её нет в базе.")
+
 @bot.message_handler(commands=['group'])
 def register_group(message):
     time_update()
-
-    msg = bot.reply_to(message, "Введите номер вашей группы: ")
-    bot.register_next_step_handler(msg, set_group)
+    reply = set_group(message)
+    if not reply:
+        msg = bot.reply_to(message, "Введите номер вашей группы: ")
+        bot.register_next_step_handler(msg, check_grp)
 
 
 @bot.message_handler(commands=['start'])
@@ -235,6 +257,7 @@ def start_message(message):
                           "1. /today – выдает расисание на сегодня\n"
                           "2. /tomorrow – выдает расисание на сегодня\n"
                           "3. /day – выдает расисание на день недели\n"
-                          "4. /group – записывает группу\n`Версия 3.0\nАльфа версия`", parse_mode='Markdown')
+                          "4. /group – записывает группу\n`Версия 3.1`\n`Создатель:` @A1omax", parse_mode='Markdown')
+
 
 bot.polling(none_stop=True)
